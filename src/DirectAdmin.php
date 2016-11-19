8ecf1af2032b4500591940f398e51ce3c6fdcc4d
Stijn Vanouplines <stijn@solitweb.be>
2016-11-19 11:30:53 +0100

    added post & get methods

--- src/DirectAdmin.php
+++ src/DirectAdmin.php
@@ -13,9 +13,33 @@ class LaravelDirectAdmin
         $this->connection = $connection;
     }
 
+    /**
+     * Set method to GET.
+     *
+     * @return $this
+     */
+    public function get() {
+        $this->connection->set_method('GET');
+        return $this;
+    }
+
+    /**
+     * Set method to POST.
+     *
+     * @return $this
+     */
+    public function post() {
+        $this->connection->set_method('POST');
+        return $this;
+    }
+
+    /**
+     * Do an API request.
+     *
+     * @return array result parsed
+     */
     public function request($method, $command, $options = [])
     {
-        $this->connection->set_method($method);
         $this->connection->query('/CMD_API_'.$command, $options);
         return $this->connection->fetch_parsed_body();
     }
